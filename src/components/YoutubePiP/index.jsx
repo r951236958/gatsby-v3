e3b48df0d4617dc9b4efd8a87d6b73c17d454407
Nick <r951236958@gmail.com>
2021-08-02 15:05:37 +0800

    更改 URL Params擷取方式

--- src/components/YoutubePiP/index.jsx
+++ src/components/YoutubePiP/index.jsx
@@ -7,50 +7,52 @@ const YoutubePiP = () => {
   const location = useLocation()
 
   const urlParams = new URLSearchParams(location.pathname)
-  // const path = `${props.location.pathname}?${urlParams.toString()}`
   const videoParameter = urlParams.get("v")
+  const youtubeURL = new URLSearchParams(location.search).get("v")
+
+  const getVideoFromUrl = url => {
+    // https://stackoverflow.com/a/9102270/3276759
+
+    const regExp = /^.*(youtu\.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\v=)([^#\\?]*).*/
+    const match = url.match(regExp)
+    if (match && match[2].length === 11) {
+      return match[2]
+    }
+
+    const fallbackRegex = /v=([^\\s&#]*)/
+    const fallbackMatch = url.match(fallbackRegex)
+
+    if (fallbackMatch && fallbackMatch[1]) {
+      return fallbackMatch[1]
+    }
+
+    return null
+  }
+
+  const getVideoId = () => {
+    const videoIdFromUrl = getVideoFromUrl(youtubeURL)
+    return videoIdFromUrl ? videoIdFromUrl : youtubeURL
+  }
+
+  const videoId = getVideoId()
 
   let videoWrapper
-  if (!videoParameter) {
+
+  if (videoParameter) {
     videoWrapper = (
       <div className="not-notAvailable">
         <h1>Define a video using the v query parameter</h1>
-        <p>{new URLSearchParams(location.search).get("v")}</p>
+        <p>{youtubeURL}</p>
+        <p>VideoID: {videoId}</p>
       </div>
     )
   } else {
-    const getVideoFromUrl = url => {
-      // https://stackoverflow.com/a/9102270/3276759
-
-      const regExp = /^.*(youtu\.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\v=)([^#\\?]*).*/
-      const match = url.match(regExp)
-      if (match && match[2].length === 11) {
-        return match[2]
-      }
-
-      const fallbackRegex = /v=([^\\s&#]*)/
-      const fallbackMatch = url.match(fallbackRegex)
-
-      if (fallbackMatch && fallbackMatch[1]) {
-        return fallbackMatch[1]
-      }
-
-      return null
-    }
-
-    const getVideoId = () => {
-      const videoIdFromUrl = getVideoFromUrl(videoParameter)
-      return videoIdFromUrl ? videoIdFromUrl : videoParameter
-    }
-
-    const videoId = getVideoId()
-
     videoWrapper = (
       <div className="youtube-PiP">
         <iframe
           width="560"
           height="315"
-          src="https://www.youtube.com/embed/${videoId}"
+          src={`https://www.youtube.com/embed/${videoId}`}
           frameborder="0"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
           allowfullscreen
@@ -58,15 +60,6 @@ const YoutubePiP = () => {
       </div>
     )
   }
-
-  //   if (!videoParameter) {
-  //     const notAvailable = () => (
-  //       <div className="not-notAvailable">
-  //         Define a video using the v query parameter
-  //       </div>
-  //     )
-  //     return
-  //   }
 
   return <div className="video-wrapper">{videoWrapper}</div>
 }
